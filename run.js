3f7733b539a275967fdbd64e4115b76cf925a6fd
girolle <girolle@github.com>
2021-02-26 12:11:20 +0300

    new test

---
+++ run.js
@@ -0,0 +1,28 @@
+const stylelint = require('stylelint');
+const fs = require('fs');
+const path = require('path');
+
+const stylelint_opt = () => {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, 'stylelint.json'), 'utf-8').toString());
+}
+
+async function useLint(projectPath) {
+    const data = await stylelint.lint({
+        files: projectPath + '**/*.css',
+        config: stylelint_opt()
+    });
+    const errors = [];
+    for (let result of data.results) {
+        for (let warning of result.warnings) {
+            errors.push(warning);
+        }
+    }
+    return errors;
+}
+
+async function test() {
+    const lint = await useLint('');
+    console.log(lint);
+}
+
+test();
---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#wait $(lsof -t -i:3000) 2>/dev/null
+#kill $(lsof -t -i:3000)
+#KILL=$(echo $$)
+#echo "$KILL"
+
+START_ERR="NO START SCRIPT"
+PARCEL_ERR="NO PARCEL IN START SCRIPT"
+PORT_ERR="NOT 3000 PORT"
+
+function print_err(){
+	echo -e "\e[31m$@\e[0m"
+}
+
+START=$(npm run | grep "start" -A1)
+if [[ -z $START ]]
+then
+	print_err $START_ERR
+	exit 0
+fi
+
+PARCEL=$(npm run | grep "start" -A1 | grep "parcel")
+if [[ -z $PARCEL ]]
+then
+	print_err $PARCEL_ERR
+	exit 0
+fi
+
+npm -s run start &
+sleep 5
+PORT=$(netstat -an | grep ':3000 ')
+if [[ -z $PORT ]]
+then
+	print_err $PORT_ERR
+fi
+killall node
+node run.js
+exit 0
