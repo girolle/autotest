e64e98d610b2084032a23d1b4059df468ff14991
girolle <girolle@github.com>
2021-02-26 15:37:38 +0300

    trying to devide checkers 2

--- check_port.sh
+++ check_port.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-. /autotest/print_err.sh
+. print_err.sh
+
+pwd
 
 PORT_ERR="NOT 3000 PORT"
 
-npm i && npm -s run start &
+npm i > /dev/null && npm -s run start &
 sleep 20
 PORT=$(netstat -an | grep ':3000 ')
 if [[ -z $PORT ]]
 then
 	print_err $PORT_ERR
-	exit 0
+	exit 1
 fi
+
+exit 0
